17ef97bd7dddaa8b33f056e520e689e58c17c9c5
mohammad-tayeb <mohammad.tayeb9010@gmail.com>
2023-11-18 23:08:47 +0600

    string Case added

--- findingSimpleInterest.js
+++ findingSimpleInterest.js
@@ -1,9 +1,9 @@
-// function findInterest(principle,time,rate){
-//     interest = principle * time * rate / 100; 
-//     return interest;
-// }
-// let simpleInterest = findInterest(2000,1,10);
-// console.log(simpleInterest);
+function findInterest(principle,time,rate){
+    interest = principle * time * rate / 100; 
+    return interest;
+}
+let simpleInterest = findInterest(2000,1,10);
+console.log(simpleInterest);
 
 let interest = Math.pow(2,3);
 console.log(interest);
---
+++ stringOperations.js
@@ -0,0 +1,4 @@
+let myName = 'Tayeb';
+console.log(myName);
+let firstLatter = myName[0];
+console.log(firstLatter); 
