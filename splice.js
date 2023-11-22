89a0fa8583fd7d7455831f01b23777a117a71763
mohammad-tayeb <mohammad.tayeb9010@gmail.com>
2023-11-22 19:09:08 +0600

    splice added

--- splice.js
+++ splice.js
@@ -5,3 +5,7 @@ let demoArray = [1,2,3,4,5,6,7];
 let newArray = demoArray.splice(2,2,1);
 console.log(newArray);
 console.log(demoArray);
+// from an array we can find a value using includes
+let demoArray2 = [44, 10, 20, 66, 18, 88];
+let find = demoArray2.includes(10);
+console.log(find);
